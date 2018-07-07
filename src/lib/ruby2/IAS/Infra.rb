707ea03506c557cb6c713085860681604d9846bd
Martin VanWinkle III <martin.vanwinkle@gmail.com>
2018-07-07 12:01:42 -0700

    progress

--- src/lib/ruby2/IAS/Infra.rb
+++ src/lib/ruby2/IAS/Infra.rb
@@ -4,10 +4,14 @@ class IAS::Infra; end
 
 require 'IAS/Infra/Logger'
 require 'IAS/Infra/FindBin'
+require 'IAS/Infra/FullProjectPaths'
 
 class IAS::Infra
+
 	include IAS::Infra::Logger
 	include IAS::Infra::FindBin
+	include IAS::Infra::FullProjectPaths
+	
 	@@debug_IAS_Infra = true;
 	def run()
 		# Admittedly, I don't know the best way
@@ -28,10 +32,12 @@ class IAS::Infra
 	
 	def debug_IAS_infra()
 		self.debug_FindBin()
+		self.debug_FullProject_Paths
 	end
 	
 	def log_start
-		self.log_info("Startin Ruby application\n")
+		# TODO: Make this log begin date, end date, options, etc
+		self.log_info("Starting Ruby application\n")
 	end
 	
 	def log_end
--- src/lib/ruby2/IAS/Infra/FullProjectPaths.rb
+++ src/lib/ruby2/IAS/Infra/FullProjectPaths.rb
@@ -3,12 +3,27 @@
 module IAS; end
 module Infra; end
 
+# require 'IAS/Infra/FindBin'
+
 module IAS::Infra::FullProjectPaths
 
-	@bin_whence = 'RealBin'
+	# include IAS::Infra::FindBin
+	@@bin_whence = 'RealBin'
 	
-	@bin_whences = {
-		'RealBin' => 'somesubpointer',
-	} 
+	def init_whences
+		@@bin_whences = {
+			'RealBin' => self.RealBin(),
+			'Bin' => self.Bin(),
+		}
+	end
+	
+	def get_whence_dir
+		self.init_whences()
+		return @@bin_whences[@@bin_whence]
+	end
 
+	def debug_FullProject_Paths
+		puts "Bin whence: "
+		puts self.get_whence_dir()
+	end
 end
