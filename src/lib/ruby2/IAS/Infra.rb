e6bd6c6fa4cb5b5332044144747ba4b95e684fd0
Martin VanWinkle III <martin.vanwinkle@gmail.com>
2018-07-07 09:36:22 -0700

    Findbin.

--- src/lib/ruby2/IAS/Infra.rb
+++ src/lib/ruby2/IAS/Infra.rb
@@ -3,13 +3,22 @@ module IAS; end
 class IAS::Infra; end
 
 require 'IAS/Infra/Logger'
+require 'IAS/Infra/FindBin'
 
 class IAS::Infra
 	include IAS::Infra::Logger
+	include IAS::Infra::FindBin
 	def run()
-		setup_IAS_infra_logging()
-		puts "Running your application.\n"
+		self.setup_IAS_infra_logging()
+		self.setup_IAS_infra_FindBin()
+		self.log_info("Startin Ruby application\n")
+		self.debug_IAS_infra()
 		self.main()
+		self.log_info("Ending Ruby application\n")
+	end
+	
+	def debug_IAS_infra()
+		self.debug_FindBin()
 	end
 end
 
---
+++ src/lib/ruby2/IAS/Infra/FindBin.rb
@@ -0,0 +1,37 @@
+
+module IAS; end
+module Infra; end
+
+module IAS::Infra::FindBin
+	def setup_IAS_infra_FindBin()
+		@dollar_zero = $0
+	end
+
+	def Script
+		return File.basename(@dollar_zero)
+	end	
+	
+	def Bin
+		return File.expand_path(File.dirname(@dollar_zero))
+	end
+	
+	def RealScript
+		return File.realpath(@dollar_zero)
+	end
+	
+	def RealBin
+		File.dirname(File.realpath(@dollar_zero))
+	end
+	
+	def debug_FindBin
+		puts "Script:"
+		puts self.Script()
+		puts "Bin:"
+		puts self.Bin()
+		puts "RealScript:"
+		puts self.RealScript()
+		puts "RealBin:"
+		puts self.RealBin()
+	end
+end
+
